2b3af2973005ba9edff7aef540b1cf3c7855e495
aarsh10agrawal <aarsh2345@gmail.com>
2025-07-01 10:49:36 +0530

    Added : Program To Find Area Of Circle

---
+++ 1stsheet/AreaOfaCircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    int area;
+	    Scanner sc = new Scanner(System.in);
+	    int radius = sc.nextInt();
+		area= 22/7 * radius^2;
+	    System.out.print(area);
+
+	}
+}
